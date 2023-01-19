f44d3db7b34a5a98b806a0180c74ee6076e8a349
miniopl802222@gmail.com <110758891+RommieQueen@users.noreply.github.com>
2023-01-19 23:03:20 +0900

    for, forEach

---
+++ js3_for.js
@@ -0,0 +1,12 @@
+for (let i=0; i <= 10; i++) {
+    console.log("당신의 호감도 "+i+"/10")
+}
+console.log("사랑합니다!!")
+
+
+// 배열과 foreach를 이용한 반복문!! //
+arr = [10,23,45,67,100]
+
+arr.forEach(element => {
+    console.log("당신의 호감도"+element+"/100")
+});
