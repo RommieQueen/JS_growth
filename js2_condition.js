7cade1b35fee0034b7521715db6d7e27bf63f8d9
miniopl802222@gmail.com <110758891+RommieQueen@users.noreply.github.com>
2023-01-19 22:52:45 +0900

    조건문 if else 문

---
+++ js2_condition.js
@@ -0,0 +1,12 @@
+let money = 5000
+
+if (money > 3000) {
+    console.log("Let's take a taxi.");
+}
+
+if (money > 6000) {
+    console.log("Let's take a train.");
+}
+else {
+    console.log("Let's take a bus.")
+}
