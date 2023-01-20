f80d67bb0bdfe37ad6eeb659d63d4da0c9ce8170
miniopl802222@gmail.com <110758891+RommieQueen@users.noreply.github.com>
2023-01-20 11:29:01 +0900

    switch case 문

---
+++ js7_switch_case.js
@@ -0,0 +1,19 @@
+let q = prompt("상품코드를 입력해주세요. 1~5")
+
+switch(q){
+    case '1' :
+        console.log('고먐미 / 네모네모빔 특화 / 500원')
+        break
+    case '2' :
+        console.log('멈뭄미 / 귀욥따 / 500원')
+        break
+    case '3' :
+        console.log('은성이의 사랑 / 행복해진다 / 무료!')
+        break
+    case '4' :
+        console.log('책 / 무슨책인지 모른다 / 100원')
+        break
+    case '5' :
+        console.log('위스키 한 잔 / 30년산 / 800000000원')
+        break
+}
