d7dbef3f4eca2230be4c256d198c24a1f62c1e40
miniopl802222@gmail.com <110758891+RommieQueen@users.noreply.github.com>
2023-01-20 15:18:03 +0900

    함수

--- js1_function.js
+++ js1_function.js
@@ -9,3 +9,23 @@ add1 = (x1,y1) => { //이렇게도 선언 가능
 }
 
 ((x2, y2) => {return x2+y2;})(5,6) //함수 감추기
+
+function showName(name) {
+    const msg = `Hi, ${name}`; //백틱, 달러 이용
+    console.log(msg)
+}
+showName('jes')
+showName('bitch')
+showName() //undefined
+showName('')// Hi, 만 출력
+
+// 이름이 입력되었으면 이름까지, 아니면 hello!만 출력
+function showName2(name2){
+    console.log(name2)
+    let msg2 = `Hello!`;
+    if(name2) { //undefined는 false다!
+        msg2 += ' '+= name2
+    }
+    console.log(msg2)
+}
+showName2('zzizzi')
