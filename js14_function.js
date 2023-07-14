973f40daf4303c6606676a23e0b2a526e098eef0
ROMMIEQUEEN <miniopl802222@gmail.com>
2023-07-14 12:02:40 +0900

    가변매개변수, 콜백함수 추가

---
+++ js14_function.js
@@ -0,0 +1,23 @@
+//익명함수
+const f = function(){
+    console.log('함수 1')
+    console.log('함수 2')
+    console.log('함수 3')
+}
+
+f()
+
+console.log(typeof p)
+console.log(f) // undifined
+console.log('')
+
+//선언적 함수
+function f1(){
+    console.log('함수 4')
+    console.log('함수 5')
+    console.log('함수 6')
+}
+
+f1()
+console.log(typeof f1) //function
+console.log(f1)
